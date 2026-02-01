ca5786c9389d959c9a4e9d8e734c51e16a510c63
CNinaH <christina3.hansen@umconnect.umt.edu>
2026-02-01 11:37:31 -0700

    update

--- Homework/homework03/script.js
+++ Homework/homework03/script.js
@@ -1,12 +1,12 @@
-console.log("Three Favorite Website?")
-console.log("YouTube, Pintrest and Spotify.")
+console.log("Three Favorite Website?");
+console.log("YouTube, Pintrest and Spotify.");
 
-console.log("Three Favorite Games?")
-console.log("Ghost of Tsushima, Ledgend of Zelda Breathe of the Wild and Clair Obscure: Expidition 33")
+console.log("Three Favorite Games?");
+console.log("Ghost of Tsushima, Ledgend of Zelda Breathe of the Wild and Clair Obscure: Expidition 33");
 
-console.log("Three Favorite Artists and their cultural importance?")
-console.log("Hayo Miyazaki famous animator at Studio Ghibli, Qinniart water color artisits, she has since passed away and Lord Gris instagram artist.")
+console.log("Three Favorite Artists and their cultural importance?");
+console.log("Hayo Miyazaki famous animator at Studio Ghibli, Qinniart water color artisits, she has since passed away and Lord Gris instagram artist.");
 
-var name = prompt("What is your favorite song?")
+var name = prompt("What is your favorite song?");
 
-console.log("Hello, "+ song)
+console.log("Your favorite color is" + song);
